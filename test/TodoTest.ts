3a9154252317aaded2d9148b2e0063140130989e
signor1 <emmanuelomemgboji@gmail.com>
2024-02-13 18:49:00 +0100

    feat: added todo retrieval

--- test/TodoTest.ts
+++ test/TodoTest.ts
@@ -1,7 +1,4 @@
-import {
-  loadFixture,
-} from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
@@ -10,13 +7,16 @@ describe("Testing Todos", function () {
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployTodo() {
-    
     const Todo = await ethers.getContractFactory("Todos");
     const todo = await Todo.deploy();
     return { todo, Todo };
   }
 
-  
+  //constant
+  const title = "Read",
+    desc = "I want to read in the evening",
+    title2 = "Cooking",
+    desc2 = "Will be cooking today";
 
   describe("Deployment", function () {
     it("Should check if contract was deployed", async function () {
@@ -26,123 +26,142 @@ describe("Testing Todos", function () {
 
     it("Should check if a new todo is created", async function () {
       const { todo } = await loadFixture(deployTodo);
-      //constants for testing
-      const title = "Read", desc = "I want to read in the evening", title2 = "Cooking", desc2 = "Will be cooking today";
+
       const tx = await todo.addTodoItem(title, desc);
       const tx2 = await todo.addTodoItem(title2, desc2);
       expect(await todo.getTodoLen()).to.equal(2);
     });
+  });
 
+  describe("Checking Item Remove Instance", function () {
     it("Should check if the remove todo item is correct", async function () {
       const { todo } = await loadFixture(deployTodo);
 
-      const title = "Read", desc = "I want to read in the evening",
-        title2 = "Cooking", desc2 = "Will be cooking today";
       const tx = await todo.addTodoItem(title, desc);
       const tx2 = await todo.addTodoItem(title2, desc2);
       const del = await todo.removeTodoItem(0);
       expect(await todo.getTodoLen()).to.equal(1);
     });
-    
+  });
 
-    // it("Should fail if the unlockTime is not in the future", async function () {
-    //   // We don't use the fixture here because we want a different deployment
-    //   const latestTime = await time.latest();
-    //   const Lock = await ethers.getContractFactory("Lock");
-    //   await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-    //     "Unlock time should be in the future"
-    //   );
-    // });
+  describe("Checking retrieved todo instance", function () {
+    it("Should check if the returned todo is correct ", async function () {
+      const { todo } = await loadFixture(deployTodo);
+
+      const tx = await todo.addTodoItem(title, desc);
+
+      await todo.addTodoItem(title, desc);
+
+      const addedTodoIndex = 0;
+      const retrievedTodo = await todo.getTodoItem(addedTodoIndex);
+
+      expect(retrievedTodo.title).to.equal(title);
+      expect(retrievedTodo.description).to.equal(desc);
+      expect(retrievedTodo.isDone).to.equal(false);
+    });
+
+    it("Should retrieve all todos correctly", async function () {
+      // Load the TodoList contract from the fixture
+      const { todo } = await loadFixture(deployTodo);
+
+      await todo.addTodoItem(title, desc);
+      await todo.addTodoItem(title2, desc2);
+
+      // Get all todos
+      const allTodos = await todo.getTodos();
+
+      // Check if the number of todos is correct
+      expect(allTodos.length).to.equal(2);
+
+      // Check if each todo item has correct values
+      expect(allTodos[0].title).to.equal(title);
+      expect(allTodos[0].description).to.equal(desc);
+      expect(allTodos[0].isDone).to.equal(false); // Assuming the default value is false
+
+      expect(allTodos[1].title).to.equal(title2);
+      expect(allTodos[1].description).to.equal(desc2);
+      expect(allTodos[1].isDone).to.equal(false); // Assuming the default value is false
+    });
+  });
+
+  describe("Update Title & Description Instance", function () {
+    it("Should check if the update title was updated", async function () {
+      const { todo } = await loadFixture(deployTodo);
+
+      const tx = await todo.addTodoItem(title, desc);
+
+      // Get the index of the added todo item
+      const addedTodoIndex = 0;
+      const updatedTitle = "Signor Dev";
+
+      //updating the title by index
+      await todo.updateTodoTitle(updatedTitle, addedTodoIndex);
+
+      // Get the todo item after updating
+      const updatedTodo = await todo.getTodoItem(addedTodoIndex);
+
+      expect(updatedTodo.title).to.equal(updatedTitle);
+    });
+
+    it("Should check if the update description was updated", async function () {
+      const { todo } = await loadFixture(deployTodo);
+
+      const tx = await todo.addTodoItem(title, desc);
+
+      // Get the index of the added todo item
+      const addedTodoIndex = 0;
+      const updatedDesc = "I am not going out today";
+
+      //updating the title by index
+      await todo.updateTodoDesc(updatedDesc, addedTodoIndex);
+
+      // Get the todo item after updating
+      const updatedTodo = await todo.getTodoItem(addedTodoIndex);
+
+      expect(updatedTodo.description).to.equal(updatedDesc);
+    });
+
+    it("Should check if the status was updated", async function () {
+      const { todo } = await loadFixture(deployTodo);
+
+      const tx = await todo.addTodoItem(title, desc);
+
+      // Get the index of the added todo item
+      const addedTodoIndex = 0;
+
+      //updating the title by index
+      await todo.updateTodoStatus(addedTodoIndex);
+
+      // Get the todo item after updating
+      const updatedTodo = await todo.getTodoItem(addedTodoIndex);
+
+      expect(updatedTodo.isDone).to.equal(true || false);
+    });
   });
 
   describe("Reverted transaction instance", function () {
     it("Update title should fail on wrong index input", async function () {
       const { todo } = await loadFixture(deployTodo);
       const tx = todo.updateTodoTitle("Working", 3);
-      await expect(tx).to.be.revertedWith('You entered an incorrect value');
+      await expect(tx).to.be.revertedWith("You entered an incorrect value");
     });
 
     it("Update description should fail on wrong index input", async function () {
       const { todo } = await loadFixture(deployTodo);
       const tx = todo.updateTodoDesc("I will work today", 3);
-      await expect(tx).to.be.revertedWith('You entered an incorrect value');
+      await expect(tx).to.be.revertedWith("You entered an incorrect value");
     });
 
     it("Update status should fail on wrong index input", async function () {
       const { todo } = await loadFixture(deployTodo);
       const tx = todo.updateTodoStatus(3);
-      await expect(tx).to.be.revertedWith('You entered an incorrect value');
+      await expect(tx).to.be.revertedWith("You entered an incorrect value");
     });
 
     it("Remove todo item by index should fail on wrong index input", async function () {
       const { todo } = await loadFixture(deployTodo);
       const tx = todo.removeTodoItem(4);
-      await expect(tx).to.be.revertedWith('You entered an incorrect value');
+      await expect(tx).to.be.revertedWith("You entered an incorrect value");
     });
-  })
-  // describe("Withdrawals", function () {
-  //   describe("Validations", function () {
-  //     it("Should revert with the right error if called too soon", async function () {
-  //       const { lock } = await loadFixture(deployOneYearLockFixture);
-
-  //       await expect(lock.withdraw()).to.be.revertedWith(
-  //         "You can't withdraw yet"
-  //       );
-  //     });
-
-  //     it("Should revert with the right error if called from another account", async function () {
-  //       const { lock, unlockTime, otherAccount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       // We can increase the time in Hardhat Network
-  //       await time.increaseTo(unlockTime);
-
-  //       // We use lock.connect() to send a transaction from another account
-  //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-  //         "You aren't the owner"
-  //       );
-  //     });
-
-  //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-  //       const { lock, unlockTime } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       // Transactions are sent using the first signer by default
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).not.to.be.reverted;
-  //     });
-  //   });
-
-    // describe("Events", function () {
-    //   it("Should emit an event on withdrawals", async function () {
-    //     const { lock, unlockTime, lockedAmount } = await loadFixture(
-    //       deployOneYearLockFixture
-    //     );
-
-    //     await time.increaseTo(unlockTime);
-
-    //     await expect(lock.withdraw())
-    //       .to.emit(lock, "Withdrawal")
-    //       .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-    //   });
-    // });
-
-  //   describe("Transfers", function () {
-  //     it("Should transfer the funds to the owner", async function () {
-  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).to.changeEtherBalances(
-  //         [owner, lock],
-  //         [lockedAmount, -lockedAmount]
-  //       );
-  //     });
-  //   });
-  // });
+  });
 });
